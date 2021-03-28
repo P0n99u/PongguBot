e4840504957fbf1895509195bcc13cf0e12e63e4
P0n99u <81376823+P0n99u@users.noreply.github.com>
2021-03-29 02:37:06 +0900

    Update maple_bot.py

--- maple_bot.py
+++ maple_bot.py
@@ -8,7 +8,7 @@ client = discord.Client()
     
 @client.event
 async def on_ready():
-    a=discord.Game("!help |금요일 10시 요정 ")
+    a=discord.Game("!help 10시 요정 ")
     await client.change_presence(status=discord.Status.online,activity=a)
     print(client.user.name)
     print(client.user.id)
@@ -18,7 +18,7 @@ async def on_message(message):
     if message.author == client.user:
         return None
     if message.content.startswith("!help"):
-        await message.channel.send("!금요일")
+        await message.channel.send("!금요일 !로또")
     if message.content.startswith("!금요일"):
         s=ScrapEventBroad.GetSundayInfo()
         await message.channel.send(s)
