5b8adfda0bef548671cc265769d384364d4a04ca
P0n99u <81376823+P0n99u@users.noreply.github.com>
2021-03-31 03:16:40 +0900

    Update Lotto.py

--- Lotto.py
+++ Lotto.py
@@ -2,11 +2,13 @@ import random
 
 def Get_Lotto():
     s=""
-    lotto_numbers = random.sample(range(1,46),7)
+    lotto_numbers = random.sample(range(1,46),30)
     lotto_numbers.sort()
 
-    for i in range(0,6):
+    for i in range(0,30):
+        if(i>=5):
+           s= s + "\n"
         
         s= s+f" {lotto_numbers[i]}"
-        
+
     return s
