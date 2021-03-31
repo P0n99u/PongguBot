44a06753e49a6b71604f0958fa4eacb16f3671e6
P0n99u <81376823+P0n99u@users.noreply.github.com>
2021-03-31 17:03:50 +0900

    Update Lotto.py

--- Lotto.py
+++ Lotto.py
@@ -1,12 +1,17 @@
 import random
 
 def Get_Lotto():
-    s="☆★☆★ 로또 번호  ★☆★☆\n─────────────\n"
-    loto_num = []
+    loto_num=[]
+    for i in range(1,31):
+        loto_num.append(random.randint(1,46))
+    
+    s="☆★☆★ 로또 번호  ★☆★☆\n───────────────────\n"
     for i in range(0,5):
         for j in range(0,6):
-            loto_num[i*5+j]=random.randint(1,46)
-            s= s + f"{loto_num[i+5*j]}"
+            s= s+f" [{loto_num[i+5*j]}]"
+        s= s+ f"\n"
+    s= s+ f"───────────────────\n☆★☆ 로또 번호  ★☆★\n───────────────────\n"
+    return s
     
     s= s+ f"\n─────────────\n☆★☆ 로또 번호  ★☆★\n─────────────\n"
     return s
